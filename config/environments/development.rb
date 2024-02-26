b5da32256f970b9de48a4bf53c29098c41552961
Thiago Alencar <talencar@gmail.com>
2024-02-26 17:39:32 -0300

    build: fix permission hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,15 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts = [
+    IPAddr.new("0.0.0.0/0"), # All IPv4 addresses.
+    IPAddr.new("::/0"),      # All IPv6 addresses.
+    "localhost",
+    "subscription_service",
+    ENV["SERVER_HOST_NAME"],
+    "http://policy-service:3001/",
+    "policy-service:3001",
+  ]
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
