6ce8292145c9da5f7e404f19b4bfcc101326916a
Thiago Alencar <talencar@gmail.com>
2024-02-27 16:04:43 -0300

    feat: policy_worker dont broke with many requisitions

--- app/workers/policy_worker.rb
+++ app/workers/policy_worker.rb
@@ -11,25 +11,26 @@ class PolicyWorker
     puts "::::::::: MESSAGE RECEIVED :::::::::"
     policy_data = JSON.parse(msg)
 
-    insured = Insured.find_or_create_by(cpf: policy_data["insured"]["cpf"]) do |i|
-      i.name = policy_data["insured"]["name"]
+    ActiveRecord::Base.connection_pool.with_connection do
+      insured = Insured.find_or_create_by(cpf: policy_data["insured"]["cpf"]) do |i|
+        i.name = policy_data["insured"]["name"]
+      end
+
+      vehicle = Vehicle.find_or_create_by(plate: policy_data["vehicle"]["plate"]) do |v|
+        v.brand = policy_data["vehicle"]["brand"]
+        v.model = policy_data["vehicle"]["model"]
+        v.year = policy_data["vehicle"]["year"]
+      end
+
+      policy = Policy.create!(
+        insured_at: policy_data["insured_at"],
+        insured_until: policy_data["insured_until"],
+        insured: insured,
+        vehicle: vehicle
+      )
+      puts "######### POLICY CREATED: #{policy.id} #########"
+      ack!
     end
-
-    vehicle = Vehicle.find_or_create_by(plate: policy_data["vehicle"]["plate"]) do |v|
-      v.brand = policy_data["vehicle"]["brand"]
-      v.model = policy_data["vehicle"]["model"]
-      v.year = policy_data["vehicle"]["year"]
-    end
-
-    policy = Policy.create!(
-      insured_at: policy_data["insured_at"],
-      insured_until: policy_data["insured_until"],
-      insured: insured,
-      vehicle: vehicle
-    )
-    puts "######### POLICY CREATED: #{policy.id} #########"
-    ack!
-
   rescue StandardError => e
     ack!
     Sneakers.logger.error "ERROR: #{e.message}"
