03274f87deba14ea4fd9670948ecc8b6d7ba3def
Thiago Alencar <talencar@gmail.com>
2024-03-15 08:29:53 -0300

    feat: Add jwt token across entire application

--- Gemfile
+++ Gemfile
@@ -2,9 +2,6 @@ source "https://rubygems.org"
 
 ruby "3.3.0"
 
-# Project gems
-gem "sneakers"
-
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.2"
 
@@ -29,12 +26,15 @@ gem "tzinfo-data", platforms: %i[ windows jruby ]
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
+  gem "factory_bot_rails"
   gem "pry-byebug"
   gem "rspec-rails"
-  gem "factory_bot_rails"
 end
 
 group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
+
+gem "jwt", "~> 2.8"
+gem "sneakers"
--- app/controllers/v1/policies_controller.rb
+++ app/controllers/v1/policies_controller.rb
@@ -1,5 +1,7 @@
 class V1::PoliciesController < ApplicationController
   before_action :set_policy, only: [:show, :update, :destroy]
+  before_action :authenticate_request
+
   def index
     @policies = Policy.all
     render json: @policies.to_json(include: [:insured, :vehicle])
@@ -39,5 +41,15 @@ class V1::PoliciesController < ApplicationController
 
   def policy_params
     params.require(:policy).permit(:insured_at, :insured_until, :insured, :vechicle)
+  end
+
+  def authenticate_request
+    header = request.headers['Authorization']
+    token = header.split(' ').last if header
+    begin
+      decode_token = JWT.decode(token, 'secret', true, { algorithm: 'HS256' })
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
   end
 end
